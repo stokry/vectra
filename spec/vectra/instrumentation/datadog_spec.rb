e4068143f81862d427f586e39820aef9240302c1
Mijo Kristo <mijo@mijokristo.com>
2026-01-08 09:47:16 +0100

    Add new tests

--- spec/vectra/instrumentation/datadog_spec.rb
+++ spec/vectra/instrumentation/datadog_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "vectra/instrumentation/datadog"
 
 # Mock Datadog StatsD
 module Datadog
--- spec/vectra/instrumentation/new_relic_spec.rb
+++ spec/vectra/instrumentation/new_relic_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "vectra/instrumentation/new_relic"
 
 # Mock New Relic Agent
 module NewRelic
